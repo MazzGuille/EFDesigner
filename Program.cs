843debe191a042f42116e6b5f55b69724a36f04f
MazzGuille <mazzcode@gmail.com>
2022-05-28 00:05:26 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+namespace EFDesigner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("");
+            using (var db = new BloggingContextEntities())
+            {
+
+            }
+        }
+    }
+}
